35d8cfcc38a0d1bd0a78d80a0e63771de16ba18c
siddeshshewde <siddesh.darkeyed95@gmail.com>
2019-08-31 17:44:46 +0530

    Update Find_PI_to_the_Nth_Digit.java

--- Mega Project List/Solutions/Find PI to the Nth Digit (Java)/Find_PI_to_the_Nth_Digit.java
+++ Mega Project List/Solutions/Find PI to the Nth Digit (Java)/Find_PI_to_the_Nth_Digit.java
@@ -6,6 +6,7 @@ public class Find_PI_to_the_Nth_Digit{
 
     public static void main(String []args){
         
+        //Initialize PI as 22/7 using BigDecimal as the limit of characters is skyhigh
         BigDecimal numerator = new BigDecimal(22.0);
         BigDecimal denominator = new BigDecimal(7.0);
         
@@ -13,8 +14,10 @@ public class Find_PI_to_the_Nth_Digit{
         
         System.out.print("Enter the number of deciaml places to display : ");
         
+        //Take Input for number of decimal places.
         int i = sc.nextInt();
         
+        //Actual Calculation
         System.out.println(numerator.divide(denominator, i , BigDecimal.ROUND_UP));
 
     }
