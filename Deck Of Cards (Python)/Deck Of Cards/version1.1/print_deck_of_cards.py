0a7134e160a6692742442cdab19929be6a5a5e09
siddeshshewde <siddesh.darkeyed95@gmail.com>
2019-09-08 19:14:02 +0530

    Update print_deck_of_cards.py

--- Deck Of Cards (Python)/Deck Of Cards/version1.1/print_deck_of_cards.py
+++ Deck Of Cards (Python)/Deck Of Cards/version1.1/print_deck_of_cards.py
@@ -1,15 +1,19 @@
-
+CLUB = "\u2663"
+HEART = "\u2665"
+DIAMOND = "\u2666"
+SPADE = "\u2660"
 
 def show_deck():
 	#Defining deck of cards
 	pips = ["2","3","4","5","6","7","8","9","10","A","J","Q","K"]
-	suits = ["C","S","D","H"]
+	suits = [CLUB, HEART, DIAMOND, SPADE]
 	
-	#Printing all the deck possibilities
+	
 	for i in pips:
-		for j in suits:
-		    cards = []
-		    cards.append(i+j)
-		    print (cards)
+        for j in suits:
+            #cards = []
+            #cards.append(i+j)
+            print (i+j, end ="\t")
+        print("")
 
 show_deck()
